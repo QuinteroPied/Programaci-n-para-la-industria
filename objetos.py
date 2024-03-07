2af2e2dfad5859dcd044ef98465026e557e04e5b
QuinteroPied <carlos.quintero@udea.edu.co>
2024-03-06 19:59:38 -0500

    clase 06 03 2024

--- objetos.py
+++ objetos.py
@@ -1,6 +1,7 @@
 
 # para crear objetos la palabra clave es Class
 from typing import Any
+import math as math
 
 
 class Persona: 
@@ -25,15 +26,73 @@ class Animal:
 class Perro(Animal):
 
     def hacer_sonido(self):
-        return "Guau"
+        print("Guauuuu")
     
 class Gato(Animal):
+
     def hacer_sonido(self):
-        return "Miau"
+        print("Miauuuu")
     
-gato = Gato('Vaquita')
+gato = Gato('Vaquita') 
 perro = Perro("Yessie")
 
-print(f"Mi gato se llama {gato.nombre} y hace {gato.hacer_sonido}")
+#print(f"Mi gato se llama {gato.nombre} y hace {ato.hacer_sonido}")
 
-print(f"Mi perro se llama {perro.nombre} y hace {perro.hacer_sonido}")
+#print(f"Mi perro se llama {perro.nombre} y hace {perro.hacer_sonido}")
+#gato.hacer_sonido
+#--------------------------------------------------------------------------------------------------------------#
+
+class Figura: # Definimos la CLASE PADRE 
+    def __init__(self, color) -> None: # Definimos el o los atributos
+        self.color=color # autoretribuimos los valores
+    def calcular_area(self): # Definimos el método
+        pass
+
+class Rectangulo(Figura): # Definimos el objeto
+    def __init__(self, color, altura, base) -> None: # Definimos el atributo
+        super().__init__(color) # Así nos traemos el atributo de la CLASE PADRE
+        self.altura = altura # autoretribuimos los valores
+        self.base = base # autoretribuimos los valores
+    def calcular_area(self):   # Método
+        area = self.base*self.altura
+        return area
+    
+class Circulo(Figura): # Definimos el objeto
+    def __init__(self, color, radio) -> None: # Definimos el atributo
+        super().__init__(color) # Así nos traemos el atributo de la CLASE PADRE
+        self.radio = altura # autoretribuimos los valores
+    def calcular_area(self):   # Método
+        area = self.radio*(math.pi**2)
+        return area
+    
+class Triangulo(Figura): # Definimos el objeto
+    def __init__(self, color, altura, base) -> None: # Definimos el atributo
+        super().__init__(color) # Así nos traemos el atributo de la CLASE PADRE
+        self.altura = altura # autoretribuimos los valores
+        self.base = base # autoretribuimos los valores
+    def calcular_area(self):   # Método
+        area = (self.base*self.altura)/2
+        return area
+
+rectangulo=Rectangulo(color="rojo", altura=10, base=5)
+print(rectangulo.calcular_area())
+
+#-----------------------------------------------------------------------------------------------------------------#
+
+class Cuenta_Bancaria:
+    def __init__(self, titular, saldo_inicial) -> None:
+        self.titular=titular
+        self._saldo=saldo_inicial
+        
+    def depositar(self, cantidad):
+        self._saldo += cantidad
+        print('el saldo actual es de '+str(self._saldo))
+    def retirar(self, cantidad):
+        if cantidad >0 and cantidad <= self._saldo:
+            self._saldo -= cantidad
+            print('retiro exitoso, el saldo actual es: '+str(self._saldo))
+
+    def obtener_saldo(self):
+        return self._saldo
+    
+    cuenta_carlos=Cuenta_Bancaria(titular='Carlos Quintero', saldo_inicial=5000)
